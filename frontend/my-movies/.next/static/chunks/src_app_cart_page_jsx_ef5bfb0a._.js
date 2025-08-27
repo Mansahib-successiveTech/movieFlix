1b2da51c0df217cb26dea03e1095477e689278d9
Mansahib-successiveTech <mansahib.singh@successive.tech>
2025-08-27 11:22:34 +0530

    Delete frontend/my-movies/.next directory

--- frontend/my-movies/.next/static/chunks/src_app_cart_page_jsx_ef5bfb0a._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_afeac031._.js"
-],
-    source: "dynamic"
-});
